cae3028094200f912fad313989609a469a01bc38
Pierce Freeman <piercefreeman@gmail.com>
2022-09-06 16:13:40 +0200

    Push README to DockerHub

---
+++ actions/README.md
@@ -0,0 +1,3 @@
+Collection of github actions used to test and build our docker containers.
+
+For security, we fork these from their original sources unless they are published by an official organization (docker/node/etc).
---
+++ actions/dockerhub-description/src/main.ts
@@ -0,0 +1,40 @@
+import * as core from '@actions/core'
+import * as inputHelper from './input-helper'
+import * as dockerhubHelper from './dockerhub-helper'
+import * as fs from 'fs'
+import {inspect} from 'util'
+
+async function run(): Promise<void> {
+  try {
+    const inputs = inputHelper.getInputs()
+    core.debug(`Inputs: ${inspect(inputs)}`)
+
+    inputHelper.validateInputs(inputs)
+
+    // Fetch the readme content
+    const readmeContent = await fs.promises.readFile(inputs.readmeFilepath, {
+      encoding: 'utf8'
+    })
+
+    // Acquire a token for the Docker Hub API
+    core.info('Acquiring token')
+    const token = await dockerhubHelper.getToken(
+      inputs.username,
+      inputs.password
+    )
+    // Send a PATCH request to update the description of the repository
+    core.info('Sending PATCH request')
+    await dockerhubHelper.updateRepositoryDescription(
+      token,
+      inputs.repository,
+      inputs.shortDescription,
+      readmeContent
+    )
+    core.info('Request successful')
+  } catch (error: any) {
+    core.debug(inspect(error))
+    core.setFailed(error.message)
+  }
+}
+
+run()
